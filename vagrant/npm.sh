e7e8f1ae52ee126faf99e661514dc4e1f36756fd
LandRover <github.com@landroverz.com>
2020-06-02 15:24:48 +0300

    replaced echo with tee

--- vagrant/npm.sh
+++ vagrant/npm.sh
@@ -4,7 +4,7 @@ if ! command -V npm >/dev/null 2>&1; then
     sudo apt install -q -y  nodejs
     
     if grep -Fxq "node_modules" ~/.profile ; then
-        echo "export PATH=\$PATH:node_modules/.bin" >> ~/.profile
+        echo "export PATH=\$PATH:node_modules/.bin" | tee -a ~/.profile
 
         echo "[v] .profile path node_modules/.bin added.";
     else
