963e8891214cf6c400f87326af981bb10d81a4be
LandRover <github.com@landroverz.com>
2020-06-02 21:07:08 +0300

    webassets updated to latest

--- webassets/webpack.config.js
+++ webassets/webpack.config.js
@@ -52,6 +52,7 @@ module.exports = (env, options) => {
                   {
                     loader: 'file-loader',
                     options: {
+                        esModule: false,
                         name(file) {
                             if (isDevMode) {
                                 return 'assets/[path][name].[ext]';
