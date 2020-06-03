a17ba0efa2efa763bc3c0f3420ebb2b6e6617ed6
LandRover <github.com@landroverz.com>
2020-06-03 10:58:06 +0300

    fixed protobuf.sh generating scripts, for latest protobuf

--- ci/protobuf.sh
+++ ci/protobuf.sh
@@ -1,23 +1,43 @@
 #!/bin/bash
+
 set -e
 
 # this file handles recompilation of files after a protocol buffer
 # file gets modified
 
-# HACK- generate non-gogo proto for gateway due to bugs
+
+# VERSION
+GRPC_GATEWAY_VERSION="v1.14.6";
+GOGO_PROTOBUF_VERSION="v1.3.1";
+
+# PATHS
+GO_PKG_PATH="${GOPATH}/pkg/mod/";
+GRPC_PATH="${GO_PKG_PATH}/github.com/grpc-ecosystem/grpc-gateway";
+GOGO_PROTOBUF_PATH="${GO_PKG_PATH}/github.com/gogo/protobuf"
+
+
+# Links to versions
+## GRPC
+[ -d "${GRPC_PATH}" ] && rm -Rf ${GRPC_PATH}; ln -s "${GRPC_PATH}@${GRPC_GATEWAY_VERSION}" ${GRPC_PATH}
+
+## GOGO PROTOBUF
+[ -d "${GOGO_PROTOBUF_PATH}" ] && rm -Rf ${GOGO_PROTOBUF_PATH}; ln -s "${GOGO_PROTOBUF_PATH}@${GOGO_PROTOBUF_VERSION}" ${GOGO_PROTOBUF_PATH}
+
+
+# HACK - generate non-gogo proto for gateway due to bugs
 # (custom types, plus gateway json encoder doesn't support time.Time')
 protoc \
     -I ./protobuf/ \
-    -I $GOPATH/pkg/mod \
-    -I $GOPATH/pkg/mod/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+    -I ${GOPATH}/pkg/mod \
+    -I ${GRPC_PATH}/third_party/googleapis \
     --go_out=Mgoogle/api/annotations.proto=google.golang.org/genproto/googleapis/api/annotations,plugins=grpc:../ \
     ./protobuf/account.proto
 mv account/account.pb.go account/api/
 
 protoc \
     -I ./protobuf/ \
-    -I $GOPATH/pkg/mod \
-    -I $GOPATH/pkg/mod/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+    -I ${GOPATH}/pkg/mod \
+    -I ${GRPC_PATH}/third_party/googleapis \
     --grpc-gateway_out=logtostderr=true:../ \
     ./protobuf/account.proto
 mv ./account/account.pb.gw.go ./account/api/
@@ -30,16 +50,16 @@ sed -i "s/package account/package main/g" account/api/account.pb.gw.go
 # model
 protoc \
     -I ./protobuf/ \
-    -I $GOPATH/pkg/mod \
-    -I $GOPATH/pkg/mod/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+    -I ${GOPATH}/pkg/mod \
+    -I ${GRPC_PATH}/third_party/googleapis \
     --gogo_out=Mgoogle/api/annotations.proto=google.golang.org/genproto/googleapis/api/annotations,plugins=grpc:../ \
     ./protobuf/account.proto
 # gateway
 # swagger
 protoc \
     -I ./protobuf/ \
-    -I $GOPATH/pkg/mod \
-    -I $GOPATH/pkg/mod/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+    -I ${GOPATH}/pkg/mod \
+    -I ${GRPC_PATH}/third_party/googleapis \
     --swagger_out=logtostderr=true:./account/api/ \
     ./protobuf/account.proto
 # Encode swagger
@@ -51,22 +71,22 @@ cd ../..
 # email.proto -> email
 protoc \
     -I ./protobuf/ \
-    -I $GOPATH/pkg/mod \
+    -I ${GOPATH}/pkg/mod \
     --gogo_out=plugins=grpc:.. \
     ./protobuf/email.proto
     
 # sms.proto -> sms
 protoc \
     -I ./protobuf/ \
-    -I $GOPATH/pkg/mod \
+    -I ${GOPATH}/pkg/mod \
     --gogo_out=plugins=grpc:.. \
     ./protobuf/sms.proto
 
 # bot.proto -> bot
 protoc \
     -I ./protobuf/ \
-    -I $GOPATH/pkg/mod \
-    -I $GOPATH/pkg/mod/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+    -I ${GOPATH}/pkg/mod \
+    -I ${GRPC_PATH}/third_party/googleapis \
     --gogo_out=plugins=grpc:.. \
     ./protobuf/bot.proto
 
@@ -74,16 +94,16 @@ protoc \
 # (custom types, plus gateway json encoder doesn't support time.Time')
 protoc \
     -I ./protobuf/ \
-    -I $GOPATH/pkg/mod \
-    -I $GOPATH/pkg/mod/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+    -I ${GOPATH}/pkg/mod \
+    -I ${GRPC_PATH}/third_party/googleapis \
     --go_out=Mgoogle/api/annotations.proto=google.golang.org/genproto/googleapis/api/annotations,plugins=grpc:../ \
     ./protobuf/company.proto
 mv company/company.pb.go company/api/
 
 protoc \
     -I ./protobuf/ \
-    -I $GOPATH/pkg/mod \
-    -I $GOPATH/pkg/mod/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+    -I ${GOPATH}/pkg/mod \
+    -I ${GRPC_PATH}/third_party/googleapis \
     --grpc-gateway_out=logtostderr=true:../ \
     ./protobuf/company.proto
 mv ./company/company.pb.gw.go ./company/api/
@@ -95,15 +115,15 @@ sed -i "s/package company/package main/g" company/api/company.pb.gw.go
 # model
 protoc \
     -I ./protobuf/ \
-    -I $GOPATH/pkg/mod \
-    -I $GOPATH/pkg/mod/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+    -I ${GOPATH}/pkg/mod \
+    -I ${GRPC_PATH}/third_party/googleapis \
     --gogo_out=Mgoogle/api/annotations.proto=google.golang.org/genproto/googleapis/api/annotations,plugins=grpc:../ \
     ./protobuf/company.proto
 # swagger
 protoc \
     -I ./protobuf/ \
-    -I $GOPATH/pkg/mod \
-    -I $GOPATH/pkg/mod/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+    -I ${GOPATH}/pkg/mod \
+    -I ${GRPC_PATH}/third_party/googleapis \
     --swagger_out=logtostderr=true:./company/api/ \
     ./protobuf/company.proto
 # Encode swagger
