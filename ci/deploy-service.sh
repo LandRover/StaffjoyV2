a975be4ff41a6e4cf431af157706e82ec722a8ee
LandRover <github.com@landroverz.com>
2020-06-12 15:39:07 +0300

    fixed k8s deployment scripts for k8s latest

--- ci/deploy-service.sh
+++ ci/deploy-service.sh
@@ -1,32 +1,66 @@
-echo "Starting deployment for service $service version $VERSION"
+#!/bin/bash
+
+SERVICE="$1";
+VERSION="$2";
+NAMESPACE="${3:-development}";
+
+DEPLOYMENT_CONFIG_PATH="/tmp/k8s/deployments/";
+SERVICES_PATH="./ci/k8s/${NAMESPACE}/services";
+DEPLOYMENTS_PATH="./ci/k8s/${NAMESPACE}/deployments";
+
+function usage {
+    if [ ! -z "$1" ]; then
+        echo $1;
+        echo "";
+    fi
+
+    echo "USAGE: $0 <service> <version> [namespace]";
+    echo "";
+    echo "<service> - Name of the service to deploy";
+    echo "<version> - version of build to deploy";
+    echo "[namespace] - if empty, default namespace will be ${NAMESPACE}";
+    echo "";
+
+    exit ${E_BADARGS};
+}
+
+## validation inputs
+[[ -z "${SERVICE}" || -z "${VERSION}" || -z "${NAMESPACE}" ]] && usage;
+
+echo "[v] Starting deployment for service ${SERVICE} version ${VERSION}"
 
 # Create or update service
-echo "Checking if k8s service for $service exists..."
-kubectl get service $service-service --namespace=$NAMESPACE 2>&1 >/dev/null
+echo "[v] Checking if k8s service for ${SERVICE} exists..."
+kubectl get service ${SERVICE}-service --namespace=${NAMESPACE} 2>&1 >/dev/null
 
 if [ $? -ne 0 ]
 then
-  echo "K8s service for $service doesn't exist.  Creating service..."
-  kubectl --namespace=$NAMESPACE create -f ./ci/k8s/$NAMESPACE/services/$service.yaml
+  echo "[v] K8s service for ${SERVICE} doesn't exist.  Creating service..."
+  kubectl --namespace=${NAMESPACE} create -f ${SERVICES_PATH}/${SERVICE}.yaml
 else
-  echo "K8s service for $service exists "
-  # TODO - not really clear how to update a running service
-  #kubectl --namespace=$NAMESPACE replace -f ./ci/k8s/$NAMESPACE/services/$service.yaml
+  echo "[v] K8s service for ${SERVICE} exists "
+  # TODO - removing --force causes spec.clusterIP: Invalid value: "": field is immutable
+  kubectl --namespace=${NAMESPACE} replace -f ${SERVICES_PATH}/${SERVICE}.yaml --force
 fi
 
-cp ./ci/k8s/$NAMESPACE/deployments/$service.yaml ./ci/k8s/$NAMESPACE/deployments/$service-copy.yaml
-sed -i "s/VERSION/$VERSION/g" ./ci/k8s/$NAMESPACE/deployments/$service-copy.yaml
+[ ! -d ${DEPLOYMENT_CONFIG_PATH} ] && mkdir -p ${DEPLOYMENT_CONFIG_PATH}
 
-echo "Checking if deployment for $service exists..."
-kubectl get deployment $service-deployment --namespace=$NAMESPACE 2>&1 >/dev/null
+echo "[v] Copying service ${SERVICE} yaml template to ${DEPLOYMENT_CONFIG_PATH}"
+cp ${DEPLOYMENTS_PATH}/${SERVICE}.yaml ${DEPLOYMENT_CONFIG_PATH}/${SERVICE}-copy.yaml
+
+echo "[v] Modifing service ${SERVICE} yaml version to ${VERSION}"
+sed -i "s/VERSION/${VERSION}/g" ${DEPLOYMENT_CONFIG_PATH}/${SERVICE}-copy.yaml
+
+echo "[v] Checking if deployment for ${SERVICE} exists..."
+kubectl get deployment ${SERVICE}-deployment --namespace=${NAMESPACE} 2>&1 >/dev/null
 if [ $? -eq 0 ]
 then
-  echo "Deployment for $service exists, updating container image to version $VERSION"
-  kubectl --namespace=$NAMESPACE apply -f ./ci/k8s/$NAMESPACE/deployments/$service-copy.yaml
+  echo "[v] Deployment for ${SERVICE} exists, updating container image to version ${VERSION}"
+  kubectl --namespace=${NAMESPACE} apply -f ${DEPLOYMENT_CONFIG_PATH}/${SERVICE}-copy.yaml
 else
-  echo "Deployment for $service doesn't exist, creating deployment with container image version $VERSION"
-  kubectl --namespace=$NAMESPACE create -f ./ci/k8s/$NAMESPACE/deployments/$service-copy.yaml
+  echo "[v] Deployment for ${SERVICE} doesn't exist, creating deployment with container image version ${VERSION}"
+  kubectl --namespace=${NAMESPACE} create -f ${DEPLOYMENT_CONFIG_PATH}/${SERVICE}-copy.yaml
 fi
-echo "Finished deploying $service, version $VERSION to $NAMESPACE."
+echo "[v] Finished deploying ${SERVICE}, version ${VERSION} to ${NAMESPACE}."
 
-rm ./ci/k8s/$NAMESPACE/deployments/$service-copy.yaml
+rm ${DEPLOYMENT_CONFIG_PATH}/${SERVICE}-copy.yaml
--- ci/dev-build.sh
+++ ci/dev-build.sh
@@ -1,33 +1,35 @@
 #!/bin/bash
+
 set -e
 set -u
 set -x
 
 # This script is called by `make dev-build`
-export VERSION="dev-$(date +%s)"
-export NAMESPACE="development"
+VERSION="dev-$(date +%s)";
+NAMESPACE="development";
+
 declare -a targets=("www" "faraday" "account/api" "account/server" "email/server" "myaccount" "whoami" "company/server" "company/api" "ical" "superpowers" "sms/server" "bot/server" "app")
 
 echo "Running database migration"
-migrate -database=$ACCOUNT_MYSQL_CONFIG -path=$STAFFJOY/account/migrations/ up
-migrate -database=$COMPANY_MYSQL_CONFIG -path=$STAFFJOY/company/migrations/ up
+migrate -database=${ACCOUNT_MYSQL_CONFIG} -path=${STAFFJOY}/account/migrations/ up
+migrate -database=${COMPANY_MYSQL_CONFIG} -path=${STAFFJOY}/company/migrations/ up
 
 ## now loop through the above array
-for target in "${targets[@]}"
+for TARGET in "${targets[@]}"
 do
     # Remove slashes from service
-    service=$(echo $target | sed 's/\///g')
-    export service
+    SERVICE=$(echo ${TARGET} | sed 's/\///g')
 
     # Run the build and upload to GKE
-    /home/vagrant/.bazel/bin/bazel run //$target:docker
+    /home/vagrant/.bazel/bin/bazel run //${TARGET}:docker
     
     # Tag so we can track the deploy in Kubernetes
     # (bazel converts slash to an underscore)
-    docker tag bazel/$(echo $target):docker localhost:5000/$service:$VERSION
-    docker push localhost:5000/$service:$VERSION
+    docker tag bazel/$(echo ${TARGET}):docker localhost:5000/${SERVICE}:${VERSION}
+    docker push localhost:5000/${SERVICE}:${VERSION}
+
     # Deploy service to Kubernetes
-    ./ci/deploy-service.sh
+    ./ci/deploy-service.sh ${SERVICE} ${VERSION} ${NAMESPACE};
 done
 
-echo "Finished deploying version ${VERSION} - check the status at http://kubernetes.staffjoy-v2.local"
+echo "Finished deploying version ${VERSION} - check the status at http://kubernetes.staffjoy-v2.local";
